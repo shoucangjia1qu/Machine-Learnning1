81d01f60d6b92250cabf7b973fa37ed2fc836d22
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-10-06 23:22:07 +0800

    创建线性感知器

--- Machine Learning 1006charpter5.py
+++ Machine Learning 1006charpter5.py
@@ -40,5 +40,47 @@ plt.xlabel('迭代次数')
 plt.ylabel('误差')
 plt.show()
 
-###################逐次逼近法/迭代法求解##################
+###################线性感知器##################
+x1=np.linspace(0,9,10)
+x3=3*x1+2+np.random.rand(10)*0.2
+data1=np.array((x1,x3)).T
+y1=np.linspace(10,19,10)
+y3=3*y1+2-np.random.rand(10)*0.2
+data2=np.array((y1,y3)).T
+a=data1.tolist()
+b=data2.tolist()
+a.extend(b)
+trainSet = np.array(a)
+Bi = np.ones((20,1))
+train = np.column_stack((Bi,trainSet))          #生成训练集数据
+label = np.row_stack((np.ones((10,1)),-np.ones((10,1))))            #生成训练集标签
+'''创建单层感知器'''
+X = np.array([[1,3,3],
+              [1,4,3],
+              [1,1,1]])
+#标签
+Y = np.array([[1],
+              [1],
+              [-1]])
+
+W0 = np.zeros((3,1))
+alpha = 0.11
+steps = 500
+for i in range(steps):
+    gradient = np.dot(X,W0)     #计算梯度
+    O = np.sign(gradient)
+    Wt = W0 + alpha*np.dot(X.T,(Y-O))       #计算误差
+    W0 = Wt
+
+
+
+
+
+
+
+
+
+
+
+
 
