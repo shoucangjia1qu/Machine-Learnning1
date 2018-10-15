2d422284f9703e0488931f4775fc633281cf1cdd
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-10-16 02:20:11 +0800

    SOM

--- Machine Learning 1010 charpter6.py
+++ Machine Learning 1010 charpter6.py
@@ -159,3 +159,115 @@ plt.show()
 plt.figure()
 plt.plot(range(bp.max_iterator),bp.errorList,c='r')
 plt.show()
+
+###################SOM网络##################
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+'''定义SOM算法类'''
+class Kohonen(object):
+    '''1、定义属性'''
+    def __init__(self):
+        self.maxRate = 0.8          #最大学习率
+        self.minRate = 0.05         #最小学习率
+        self.maxRound = 5           #最大聚类半径
+        self.minRound = 0.5         #最小聚类半径
+        self.steps = 1000           #迭代次数
+        self.RateList = []          #学习率列表
+        self.RoundList = []         #聚类半径列表
+        self.w = []                 #权重
+        self.M = 2                  #输出层节点数。MxN表示聚类数，这里是展示为2*2的二维模式
+        self.N = 2                  
+        self.dataSet = 0            #训练集
+        self.Labels = 0             #自身聚类标签
+        self.Y = 0                  #聚类结果
+    '''2、读入数据集'''
+    def loadData(self,path):
+        with open(path,"r") as f:
+            content = f.readlines()
+        tempList = [row.split() for row in content]
+        m,n = np.shape(tempList)
+        data = np.zeros((m,n-1))
+        label = np.zeros((m,1))
+        for i in range(m):
+            for j in range(n):
+                if j != 0:
+                    data[i,j-1] = tempList[i][j]
+                else:
+                    label[i,0] = tempList[i][j]
+        self.dataSet = data
+        self.Labels = label
+    '''3、数据归一化/标准化'''
+    def normalize(self,dataSet):
+        m,n = np.shape(dataSet)
+        for i in range(n):
+            dataSet[:,i] = (dataSet[:,i]-np.mean(dataSet[:,i]))/np.std(dataSet[:,i]+1.0e-10)
+        return dataSet
+    '''4、定义欧氏距离公式'''
+    def edist(self,v1,v2):
+        return(np.linalg.norm(v1-v2))
+    '''5、初始化输出层/竞争层'''
+    def out_grid(self):
+        grid = np.zeros((self.M*self.N,2))      #分成四类，两个维度
+        k = 0
+        for i in range(self.M):
+            for j in range(self.N):
+                grid[k,:] = np.array([i,j])
+                k += 1
+        return grid
+    '''6、学习率和半径'''
+    def ratecalc(self,i):                                                          #学习率和半径
+    	Learn_rate = self.maxRate-((i+1.0)*(self.maxRate-self.minRate))/self.steps
+    	R_rate     = self.maxRound-((i+1.0)*(self.maxRound-self.minRound))/self.steps
+    	return  Learn_rate,R_rate
+
+    '''6、主程序'''
+    def train(self):
+        m,n = self.dataSet.shape
+        normData = self.normalize(self.dataSet)         #数据归一化
+        grid = self.out_grid()                          #输出层初始化
+        self.w = np.random.rand(n,self.M*self.N)        #随机初始化权重向量
+        if self.steps<5*m:
+            self.steps = 5*m
+        for i in range(self.steps):
+            '''计算最新的学习率和学习半径'''
+            rate,rod = self.ratecalc(i)
+            self.RateList.append(rate)
+            self.RoundList.append(rod)
+            '''随机选取样本，并找到优胜节点'''
+            k = np.random.randint(0,m)
+            tempData = normData[k,:]
+            dataDist = [self.edist(tempData,a) for a in self.w.T]
+            minIndex = dataDist.index(min(dataDist))
+            #minIndex = np.array([self.edist(tempData,i) for i in self.w.T]).argmin()
+            '''定位输出的节点位置，并计算邻域'''
+            x = np.floor(minIndex/self.M)       #下取整
+            y = np.mod(minIndex,self.M)         #取模
+            leafDist = [self.edist(np.array([x,y]),b) for b in grid]
+            rodIndex = list((np.array(leafDist)<rod).nonzero()[0])      #得到再学习半径范围内的输出节点下标
+            for d in range(self.w.shape[1]):
+                if d in rodIndex:
+                    self.w[:,d] = self.w[:,d]+rate*(mySample-self.w[:,d])
+        '''开始分类'''
+        self.Y = np.ones(m)
+        for i in range(m):
+            Ydists = [self.edist(normData[i,:],j) for j in self.w.T]
+            label = np.array(Ydists).argmin()
+            self.Y[i] = label
+
+
+'''执行程序'''
+som = Kohonen()
+som.loadData("D:\\mywork\\test\\ML\\4k2_far_data.txt")
+som.train()
+
+
+
+
+
+
+
+
+
+
