b5f8986321ea24b15b861369444aa73ee375a305
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-12-03 23:56:48 +0800

    利用矩阵方法求解最小二乘解

--- Machine Learning 1203 charpter7.py
+++ Machine Learning 1203 charpter7.py
@@ -25,9 +25,13 @@ xmean = np.mean(x)
 ymean = np.mean(y)
 a = (np.dot(x,y)-n*xmean*ymean)/(sum(np.power(x,2))-n*xmean**2)
 b = ymean - a*xmean
-
+'''计算斜率'''
+deltax = x-xmean
+deltay = y-ymean
+a1 = np.dot(deltax,deltay)/np.sum(np.power(deltax,2))
+b1 = ymean - a1*xmean
 '''画图展示'''
-preY = a*x+b
+preY = a1*x+b1
 plt.figure()
 plt.scatter(x,y)
 plt.legend(['plot'],loc=2)
@@ -36,5 +40,23 @@ plt.xlabel("X")
 plt.ylabel("Y")
 #plt.legend(['preY'],loc=4)
 plt.show()
+
+'''另一种计算方法：利用矩阵求解方式求a和b'''
+'''Mx*A=Y,求A'''
+'''Mx.T*Mx*A = Mx.T*Y'''
+'''A = (Mx.T*Mx).I*Mx.T*Y'''
+xma = np.ones((n,2))
+xma[:,1] = x
+Ex = np.dot(xma.T,xma)
+ExI = np.linalg.inv(Ex)
+A = np.dot(np.dot(ExI,xma.T),y)
+b = A[0]
+a = A[1]
+
+
+
+
+
+
 
 
