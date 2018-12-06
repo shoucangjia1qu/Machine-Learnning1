d695c0a38682a0d459b890e3de4c8d92b6c3d6e1
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-12-06 15:42:23 +0800

    RBF网络

--- Machine Learning 1203 charpter7.py
+++ Machine Learning 1203 charpter7.py
@@ -10,6 +10,11 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
+#######################
+#                     #
+#     最小二乘法       #
+#                     #
+#######################
 '''读取数据集'''
 os.chdir(r"D:\mywork\test\ML")
 with open("regdataset.txt","r") as f:
@@ -52,6 +57,84 @@ ExI = np.linalg.inv(Ex)
 A = np.dot(np.dot(ExI,xma.T),y)
 b = A[0]
 a = A[1]
+
+
+#######################
+#                     #
+#       RBF网络       #
+#                     #
+#######################
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+os.chdir(r"D:\mywork\test\ML")
+'''1、数据读入'''
+with open("nolinear.txt","r") as f:
+    content = f.readlines()
+dataList = [[float(y) for y in x.split()] for x in content]
+dataX = np.array(dataList)[:,:2]
+dataY = np.array(dataList)[:,2]
+
+'''2、设置激活函数'''
+def fuc(x,k):
+    return np.exp(np.sqrt(np.dot(x,x.T))/(-2*k**2))
+
+'''3、训练'''
+k=0.02
+m,n = np.shape(dataX)
+testY = np.zeros(m)
+for i in range(len(dataX)):
+    '''3-1训练权重W'''
+    W = np.eye(m)
+    testX = dataX[i,:]
+    for j in range(len(dataX)):
+        targetX = dataX[j,:]
+        deltaX = testX - targetX
+        W[j,j] = fuc(deltaX,k)
+    '''3-2找出斜率和截距X*A=Y'''
+    Ex = np.dot(dataX.T,np.dot(W,dataX))
+    '''3-3判断多项式是否为0'''
+    if np.linalg.det(Ex) != 0:
+        '''A = (Mx.T*Mx).I*Mx.T*Y'''
+        A = np.dot(np.dot(np.linalg.inv(Ex),dataX.T),np.dot(W,dataY))
+        '''3-4预测Y'''
+        preY = np.dot(testX,A)
+        testY[i] = preY
+'''总误差'''
+print(sum(np.power((testY-dataY),2)))
+
+'''4、画图'''
+plt.figure()
+plt.scatter(dataX[:,1],dataY,linewidths=3)
+plt.scatter(dataX[:,1],testY,c='r',linewidths=1)
+plt.plot(dataX[:,1],testY,c='g')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
