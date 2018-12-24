a59ad10ce4b5f8ab980aa58ca48fbcbeaf4093b6
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-12-24 22:40:43 +0800

    kernels
    核函数的构造

--- Machine Learning 1211 charpter8.py
+++ Machine Learning 1211 charpter8.py
@@ -15,14 +15,14 @@ import matplotlib.pyplot as plt
 #        SVM          #
 #                     #
 #######################
-'''读取数据集'''
 os.chdir(r"D:\mywork\test\ML")
 
 def PlattSVM(object):
     def __init__():
         self.trainSet = 0
         self.Labels = 0
-        
+        self.K = 0              #经核函数转变后的点积
+        self.kValue = dict()
         
     
     '''读取数据集'''
@@ -33,7 +33,48 @@ def PlattSVM(object):
         self.trainSet = OriData[:,:2]
         self.Labels = OriData[:,2]
             
-            
+     
+        
+    
+    
+    '''构造核函数'''
+    def kernels(self,data):
+        m,n = np.shape(data)
+        self.K = np.zeros(m,m)
+        for i in range(m):
+            A = data[i,:]
+            if list(self.kValue.keys())[0] == "linear":
+                self.K[i,:] = np.dot(data,A.T)
+            elif list(self.kValue.keys())[0] == "Gaussian":
+                x = np.power((data - A),2)
+                Mo = np.sqrt(np.sum(x,axis=1))
+                self.K[i,:] = np.exp(Mo/(-2*self.kValue['Gaussian']**2)
+            else:
+                raise NameError('无法识别的核函数')
+        print("核函数转换完毕")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''画图展示数据分布'''
+plt.figure()
+plt.scatter(trainSet[:100,0],trainSet[:100,1],marker='X',c='r',linewidths=1)
+plt.scatter(trainSet[100:,0],trainSet[100:,1],marker='o',c='b',linewidths=2)
+plt.show()
+
+
 
 
 
