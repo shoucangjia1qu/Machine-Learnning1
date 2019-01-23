8782109bdc4cfd46a69dfbe6985225d190220297
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2019-01-23 22:59:03 +0800

    AdaBoost完整
    算法-训练-测试-评价

--- Machine Learning 0114 charpter9.py
+++ Machine Learning 0114 charpter9.py
@@ -163,25 +163,28 @@ cv2.imwrite("face.jpg",img2)
 
 #######################
 #                     #
-# Haar casade人脸识别  #
+#  AdaBoost单层决策树  #
 #                     #
 #######################
 import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from sklearn import metrics
 os.chdir(r"D:\mywork\test\ML\郑捷《机器学习算法原理与编程实践》第2-9章节的源代码及数据集\adaboost")
 
-'''打开数据集'''
-with open("horseColicTraining.txt") as f:
-    file = f.readlines()
-data = [[float(x) for x in row.split()] for row in file]
-data = np.array(data)
-train = data[:,:-1]
-label = data[:,-1].reshape(-1,1)
-label[label==0] = -1
+'''1、封装函数'''
+'''1-1 读入数据集'''
+def loadDataSet(fileName):
+    with open(fileName) as f:
+        file = f.readlines()
+    data = [[float(x) for x in row.split()] for row in file]
+    data = np.array(data)
+    train = data[:,:-1]
+    label = data[:,-1]
+    return train,label
 
-'''AdaBoost训练'''
+'''1-2 AdaBoost训练'''
 def AdaBoostTrain(dataSet,label,iters=50):
     weakClassSet = []           #弱分类器的容器
     m,n = np.shape(dataSet)
@@ -208,7 +211,7 @@ def AdaBoostTrain(dataSet,label,iters=50):
             break
     return weakClassSet,aggValue
     
-'''单层决策树'''
+'''1-3 单层决策树'''
 def decisionTree(dataSet,label,D):
     m,n = np.shape(dataSet)
     bestFeat = dict()           #最优分隔点
@@ -239,7 +242,7 @@ def decisionTree(dataSet,label,D):
                     bestFeat['operator'] = operator
     return bestFeat,bestprelabel,minerror
 
-'''根据阈值分隔数据集'''
+'''1-4 根据阈值分隔数据集'''
 def splitDataSet(dataSet,column,threshold,operator):
     prelabel = np.ones((dataSet.shape[0],1))
     '''小于阈值的分类为0'''
@@ -251,19 +254,105 @@ def splitDataSet(dataSet,column,threshold,operator):
         prelabel[dataSet[:,column]>=threshold] = -1
     return prelabel
 
+'''1-5 预测分类'''
+def classify(dataSet,weakClassSet):
+    m,n=np.shape(dataSet)
+    aggValue = np.zeros((m,1))
+    for i in range(len(weakClassSet)):
+        prelabel = splitDataSet(dataSet,weakClassSet[i]['column'],weakClassSet[i]['threshold'],weakClassSet[i]['operator'])
+        aggValue += weakClassSet[i]['alpha']*prelabel
+    return aggValue
 
 
+'''2、正式开始训练'''
+trainSet,trainLabel = loadDataSet("horseColicTraining.txt")
+testSet,testLabel = loadDataSet("horseColicTest.txt")
+'''训练弱分类器，先将Label转换为二维数组'''
+trainLabel2D = trainLabel.reshape(-1,1)
+trainLabel2D[trainLabel2D==0] = -1
+weakClassSet,aggValue = AdaBoostTrain(trainSet,trainLabel2D,iters=10)       #10个弱分类器
+'''对测试集进行预测，并将测试集数据转为一维数组'''
+testaggValue = classify(testSet,weakClassSet)
+pretestLabel = np.sign(testaggValue)
+pretestLabel = pretestLabel.reshape(-1)
+'''查看测试集的预测效果'''
+testLabel[testLabel==0] = -1
+error = sum(pretestLabel != testLabel);errorRate = error/len(testLabel)
+print("错误:{}，错误率:{}".format(error,errorRate))
+'''测试集结果：
+错误:16，错误率:0.23880597014925373
+'''
+
+'''3、评价'''
+'''ROC画图'''
+'''第一种:书上的'''
+cur = (1,1)
+Pstep = 1/float(sum(testLabel==1));Nstep = 1/float(sum(testLabel==-1))
+tpr=[];fpr=[]
+aggValueIdx = np.argsort(testaggValue,axis=0)
+labelarr=testLabel
+plt.figure()
+for idx in (aggValueIdx):
+    if labelarr[idx] == 1:                #分错的，故真正例率要减掉
+        delP = Pstep
+        delN = 0
+    else:                               #分对的，故假正例率要减掉
+        delP = 0
+        delN = Nstep
+    plt.plot([cur[0],cur[0]-delN],[cur[1],cur[1]-delP])
+    cur = (cur[0]-delN,cur[1]-delP)
+    tpr.append(cur[1])
+    fpr.append(cur[0])
+plt.plot([0,1],[0,1],"r--")
+plt.show()
+
+'''第二种:自编的'''
+trainLabel[trainLabel==-1] = 0
+tpr = []; fpr = []; prs = []; thres = []
+Pnum = float(sum(trainLabel==1))            #正样本数量          
+Nnum = float(sum(trainLabel==0))           #负样本数量
+aggValue1D = aggValue.reshape(-1)           #先转为一维数组
+aggValueIdx = np.argsort(aggValue1D,axis=-1)       
+for idx in aggValueIdx:
+    threshold = aggValue1D[idx]     #先找到阈值
+    prelabel = aggValue1D.copy()
+    NegativeIdx = np.nonzero(prelabel<threshold)[0]
+    PostiveIdx = np.nonzero(prelabel>=threshold)[0]
+    prelabel[NegativeIdx]=0; prelabel[PostiveIdx]=1
+    TN = sum((prelabel==trainLabel)*(trainLabel==0))
+    TP = sum((prelabel==trainLabel)*(trainLabel==1))
+    FP = sum((prelabel!=trainLabel)*(trainLabel==0))
+    thres.append(threshold)
+    tpr.append(TP/Pnum)
+    fpr.append(1-(TN/Nnum))
+    prs.append(TP/(TP+FP))
+'''画图'''
+plt.plot(fpr,tpr)
+plt.plot([0,1],[0,1],"r--")
+plt.show()
+'''Area Under Curve'''
+AUCnum = 0
+PostiveIdx = np.nonzero(trainLabel==1)[0]
+for idx in PostiveIdx:
+    threshold = aggValue1D[idx]
+    Num = sum((aggValue1D<=threshold)*(trainLabel==0))
+    AUCnum += Num
+AUC = AUCnum/(Pnum*Nnum)
+AUC = metrics.roc_auc_score(trainLabel,aggValue1D)
+print("AUC:{}".format(AUC))
+AUC:0.8550468938620114
 
 
-
-
-
-
-
-
-
-
-
-
-
+'''第三种:调用sklearn'''
+'''
+fpr:1-特异率
+tpr:召回率
+'''
+fpr1, tpr1, thresholds=metrics.roc_curve(testLabel,testaggValue)
+plt.plot(fpr,tpr)
+plt.plot([0,1],[0,1],"r--")
+plt.show()
+AUC = metrics.roc_auc_score(testLabel,testaggValue)
+AUC = metrics.auc(fpr,tpr)
+'''AUC:0.8069148936170212'''
 
