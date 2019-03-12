2426dc662f45e6cf57491383afea01be8219dca3
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2019-03-12 23:55:00 +0800

    LDA

--- ML_linearDA_20190311.py
+++ ML_linearDA_20190311.py
@@ -20,12 +20,12 @@ train = np.array(df.iloc[:,1:])
 #直接sklearn实现LDA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 lda = LDA()   #选取两个主成分
-lda.fit(train,label)
-X = lda.transform(train)
+lda.fit(data,label)
+X = lda.transform(data)
 lda.explained_variance_ratio_
 '''array([0.68747889, 0.31252111])'''
 #降维后画图
-plt.scatter(xt[:,0],xt[:,1])
+plt.scatter(X[:,0],X[:,1])
 plt.show()
 
 #自编算法实现LDA
@@ -50,20 +50,17 @@ class Linerda(object):
         for i in ylabel:
             Xi = data[np.nonzero(y==i)[0]]
             Miui =  np.mean(Xi,axis=0)
-            Swi = np.dot((Xi-Miui).T,(Xi-Miui))
+            Swi = np.dot((Xi-Miui).T,(Xi-Miui))/(len(Xi)-1)
             Sw += Swi
-            Sbi = len(Xi)*np.dot((Miui-Miu),(Miui-Miu).T)
+            Sbi = len(Xi)*np.dot((Miui-Miu).reshape(1,-1).T,(Miui-Miu).reshape(1,-1))
             Sb += Sbi
-            
+        U,S,V = np.linalg.svd(Sw)
+        #选取2个变量
+        w = np.dot(Sb,np.dot(np.dot(V.T[:,:2],np.diag(S[:2])),U.T[:2,:2]))
 
+X=np.dot(data,w)
 
-
-
-
-
-
-
-
-
-
-
+plt.scatter(X[label==1,0],X[label==1,1],c='r')
+plt.scatter(X[label==2,0],X[label==2,1],c='b')
+plt.scatter(X[label==3,0],X[label==3,1],c='y')
+plt.show()
