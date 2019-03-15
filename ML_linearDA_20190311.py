8ae93942804630efad568f56bfeea1f05930429b
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2019-03-16 00:45:12 +0800

    线性判别分析课后习题3.5

--- ML_linearDA_20190311.py
+++ ML_linearDA_20190311.py
@@ -28,6 +28,7 @@ lda.explained_variance_ratio_
 plt.scatter(X[:,0],X[:,1])
 plt.show()
 
+
 #自编算法实现LDA
 class Linerda(object):
     #1、设置属性
@@ -64,3 +65,122 @@ plt.scatter(X[label==1,0],X[label==1,1],c='r')
 plt.scatter(X[label==2,0],X[label==2,1],c='b')
 plt.scatter(X[label==3,0],X[label==3,1],c='y')
 plt.show()
+
+
+
+######西瓜书3.5课后习题
+dataSet = [
+        ['青绿', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.697, 0.460, '好瓜'],
+        ['乌黑', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', 0.774, 0.376, '好瓜'],
+        ['乌黑', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.634, 0.264, '好瓜'],
+        ['青绿', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', 0.608, 0.318, '好瓜'],
+        ['浅白', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.556, 0.215, '好瓜'],
+        ['青绿', '稍蜷', '浊响', '清晰', '稍凹', '软粘', 0.403, 0.237, '好瓜'],
+        ['乌黑', '稍蜷', '浊响', '稍糊', '稍凹', '软粘', 0.481, 0.149, '好瓜'],
+        ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '硬滑', 0.437, 0.211, '好瓜'],
+        ['乌黑', '稍蜷', '沉闷', '稍糊', '稍凹', '硬滑', 0.666, 0.091, '坏瓜'],
+        ['青绿', '硬挺', '清脆', '清晰', '平坦', '软粘', 0.243, 0.267, '坏瓜'],
+        ['浅白', '硬挺', '清脆', '模糊', '平坦', '硬滑', 0.245, 0.057, '坏瓜'],
+        ['浅白', '蜷缩', '浊响', '模糊', '平坦', '软粘', 0.343, 0.099, '坏瓜'],
+        ['青绿', '稍蜷', '浊响', '稍糊', '凹陷', '硬滑', 0.639, 0.161, '坏瓜'],
+        ['浅白', '稍蜷', '沉闷', '稍糊', '凹陷', '硬滑', 0.657, 0.198, '坏瓜'],
+        ['乌黑', '稍蜷', '浊响', '清晰', '稍凹', '软粘', 0.360, 0.370, '坏瓜'],
+        ['浅白', '蜷缩', '浊响', '模糊', '平坦', '硬滑', 0.593, 0.042, '坏瓜'],
+        ['青绿', '蜷缩', '沉闷', '稍糊', '稍凹', '硬滑', 0.719, 0.103, '坏瓜']
+    ]
+#特征值列表
+labels = ['色泽', '根蒂', '敲击', '纹理', '脐部', '触感', '密度', '含糖率']
+#整理出数据集和标签
+X = np.array(dataSet)[:,6:8]
+X=X.astype(float)
+Y = np.array(dataSet)[:,8]
+Y[Y=="好瓜"]=1
+Y[Y=="坏瓜"]=0
+Y=Y.astype(float)
+
+#二分类问题
+#1、计算类内散度
+def calSw(X,Y):
+    Sw = 0
+    Miui = []
+    Swi = []
+    for i in set(Y):
+        Xi = X[Y==i,:]
+        u = np.mean(Xi,axis=0)      #计算第i类的均值
+        Miui.append(u)
+        s = np.dot((Xi-u).T,(Xi-u)) #计算第i类的协方差
+        Swi.append(s)
+        Sw += s
+    return Sw,Miui,Swi
+
+Sw,Miui,Swi = calSw(X,Y)
+#2、计算类间散度
+Sb = Miui[0] - Miui[1]
+#3、计算W向量
+U,S,V = np.linalg.svd(Sw)
+Sn = np.linalg.inv(np.diag(S))
+Swn = np.dot(np.dot(V.T,Sn),U.T)
+w = np.dot(Swn,Sb.reshape(-1,1))
+#4、求投影后的距离
+Xw = np.dot(X,w)
+X2 = np.dot(X,w)/np.linalg.norm(w)
+#5、求投影后的点
+a = w[1]/w[0]           #向量的斜率
+Xnew = np.zeros((17,2))     #投影后的点
+for i in range(17):
+    x0=np.sqrt(X2[i,0]**2/(1+a**2))
+    y0=a*x0
+    Xnew[i,0]=x0
+    Xnew[i,1]=y0
+#或者另一种算法#
+Xnew2 = np.zeros((17,2))
+for i in range(17):
+    x0 = np.dot(X[i,:],w)*w[0]/np.power(np.linalg.norm(w),2)
+    y0 = x0*a
+    Xnew2[i,0]=x0
+    Xnew2[i,1]=y0
+
+#6、画图
+plt.figure(figsize=(8,6))
+plt.scatter(X[Y==1,0],X[Y==1,1],c='g',marker='+')
+plt.scatter(X[Y==0,0],X[Y==0,1],c='r',marker='d')
+plt.plot([0,0.15],[0,0.15*a])
+plt.scatter(Xnew[Y==1,0],Xnew[Y==1,1],c='g',marker='o')
+plt.scatter(Xnew[Y==0,0],Xnew[Y==0,1],c='r',marker='o')
+for i in range(17):
+    if Y[i]==1:
+        plt.plot([X[i,0],Xnew[i,0]],[X[i,1],Xnew[i,1]],'g--')
+    else:
+        plt.plot([X[i,0],Xnew[i,0]],[X[i,1],Xnew[i,1]],'r--')
+plt.show()
+
+######如果直接用sklearn
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA()   #选取两个主成分
+lda.fit(X,Y)
+X3 = lda.transform(X)
+lda.explained_variance_ratio_
+#预测和评价
+preY = lda.predict(X)
+from sklearn import metrics
+print(metrics.classification_report(Y,preY))
+
+#降维后画图
+plt.scatter(X[:,0],X[:,1])
+plt.show()
+plt.figure(figsize=(8,6))
+plt.scatter(X[Y==1,0],X[Y==1,1],c='g',marker='+')
+plt.scatter(X[Y==0,0],X[Y==0,1],c='r',marker='d')
+plt.plot([0,0.15],[0,0.15*a])
+plt.scatter(Xnew[preY==1,0],Xnew[preY==1,1],c='g',marker='o')
+plt.scatter(Xnew[preY==0,0],Xnew[preY==0,1],c='r',marker='o')
+for i in range(17):
+    if Y[i]==1:
+        plt.plot([X[i,0],Xnew[i,0]],[X[i,1],Xnew[i,1]],'g--')
+    else:
+        plt.plot([X[i,0],Xnew[i,0]],[X[i,1],Xnew[i,1]],'r--')
+plt.scatter([Xnew[Y==1,0].mean(),Xnew[Y==0,0].mean()],[Xnew[Y==1,1].mean(),Xnew[Y==0,1].mean()],c='y',marker='D',linewidths=10)
+plt.show()
+
+
+
