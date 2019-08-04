89f630bdce312f25258563a08e044803bb93bca4
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2019-08-04 17:47:09 +0800

    Update ML_GaussianCluster_20190802.py

--- ML_GaussianCluster_20190802.py
+++ ML_GaussianCluster_20190802.py
@@ -28,7 +28,7 @@ def Gaussian_multi(x, miu, sigma):
     return:
         distributionArr:返回样本的概率分布1D数组
     """
-    distributionArr = np.exp(-0.5*np.sum(np.multiply(np.dot(x-miu, np.linalg.inv(sigma)), x-miu), axis=1))/np.power(2*np.pi, 0.5*d)*np.linalg.det(sigma)**0.5
+    distributionArr = np.exp(-0.5*np.sum(np.multiply(np.dot(x-miu, np.linalg.pinv(sigma)), x-miu), axis=1))/np.power(2*np.pi, 0.5*d)*np.linalg.det(sigma)**0.5
     return distributionArr
 
 
@@ -52,7 +52,7 @@ def Gama_Prob(x, AlphaArr, MiuArr, SigmaArr):
         GamaProbArr[:,i] = Gaussian_multi(x, miu, sigma)
     GamaProbArr = np.multiply(GamaProbArr, AlphaArr)
     SumGamaProb = np.sum(GamaProbArr, axis=1).reshape(-1,1)
-    return GamaProbArr/SumGamaProb
+    return (GamaProbArr/SumGamaProb).round(4)
 
 
 #计算似然函数
@@ -60,11 +60,31 @@ def Gama_Prob(x, AlphaArr, MiuArr, SigmaArr):
 
 
 #更新高斯分布函数参数
-
-
-
-#更新每个高斯分布函数的混合系数
-
+def updateParas(x, GamaProbArr):
+    global k, d
+    """
+    更新高斯分布函数的参数，包括均值和协方差矩阵
+    input:
+        x:样本集,m*d,其中m为样本数,d为样本维度数
+        GamaProbArr:高斯分布函数的后验概率,m*k维
+    return:
+        newMiuArr:更新后的高斯分布函数的均值,k*d维
+        newSigmaArr:更新后的高斯分布的协方差矩阵,k*d*d维
+        newAlphaArr:更新后的高斯模型的混合系数,1*k维
+    """
+    SumGamaProb = np.sum(GamaProbArr, axis=0)
+    newMiuArr = np.zeros((k,d))
+    newSigmaArr = np.zeros((k,d,d))
+    for i in range(k):
+        Gama = GamaProbArr[:,i].reshape(-1,1)
+        #更新均值
+        newMiu = np.sum(np.multiply(Gama, x), axis=0)/SumGamaProb[i]
+        newMiuArr[i] = newMiu
+        #更新协方差矩阵
+        newSigma = np.dot(np.multiply(x-newMiu, Gama).T, x-newMiu)/SumGamaProb[i]
+        newSigmaArr[i] = newSigma
+    newAlphaArr = SumGamaProb.reshape(1,-1)/m
+    return newMiuArr.round(4), newSigmaArr.round(4), newAlphaArr.round(4)
 
 
 #初始化函数参数
@@ -81,23 +101,126 @@ def initParas(x, k):
     MiuArr0 = x[np.random.randint(0, m, k)]
     #在这里固定好了
     MiuArr0 = x[[5,21,26]]
-    SigmaArr0 = np.array([[[0.1,0],[0,0.1]]]*k)
+    SigmaArr0 = np.array([(np.eye(d)*0.1).tolist()]*k)
     return AlphaArr0, MiuArr0, SigmaArr0
 
 
 #按照最终结果划分类型
+#ClusterLabel = np.argmax(GamaProbArr, axis=1)
 
 
+#计算X*Y的高斯分布
+def calXYZ(x, MiuArr, SigmaArr):
+    global m
+    """
+    画等高图需要计算X,Y,Z
+    input:
+        x:样本集,m*d,其中m为样本数,d为样本维度数
+        MiuArr, SigmaArr:高斯分布函数的参数
+    return:
+        xgrid:x的网格坐标
+        ygrid:y的网格坐标
+        zgrid:(x,y)网格坐标上高斯分布函数的概率
+    """
+    x1 = np.copy(x[:,0])
+    x1.sort()
+    y1 = np.copy(x[:,1])
+    y1.sort()
+    x2,y2 = np.meshgrid(x1,y1)  # 获得网格坐标矩阵
+    Gp = np.zeros((m,m))
+    for i in range(m):
+        for j in range(m):
+            xi = x2[i,j]
+            yi = y2[i,j]
+            data = np.array([xi,yi])
+            miuList=[]
+            for miu, sigma in zip(MiuArr, SigmaArr):   
+                p = np.exp(-0.5*np.dot(np.dot((data-miu).reshape(1,-1), np.linalg.inv(sigma)), (data-miu).reshape(-1,1)))/np.power(2*np.pi, 0.5*d)*np.linalg.det(sigma)**0.5
+                miuList.append(p)
+            Gp[i,j] = max(miuList)
+    return x2, y2, Gp
+    
 
 #画图
+def drawPics(x, MiuArr, SigmaArr, Clusters):
+    """
+    画图，不同聚类类别的点分布，等高图
+    input:
+        x:样本集,m*d,其中m为样本数,d为样本维度数
+        MiuArr, SigmaArr:高斯分布函数的参数
+        Clusters:聚类结果
+    out:
+        散点图+等高分布图
+    """
+    plt.figure(figsize=(10,6))
+    xgrid, ygrid, zgrid = calXYZ(x, MiuArr, SigmaArr)
+    c=plt.contour(xgrid,ygrid,zgrid,6,colors='black')
+    plt.contourf(xgrid,ygrid,zgrid,6,cmap=plt.cm.Blues,alpha=0.5)
+    #plt.clabel(c,inline=True,fontsize=10)
+    for i in range(k):
+        xi = x[Clusters==i,0]
+        yi = x[Clusters==i,1]
+        plt.scatter(xi, yi)
+        plt.scatter(MiuArr[i,0], MiuArr[i,1], c='r', linewidths=5, marker='D')
+    plt.show()
+    return
+
+
+#训练：判断是否符合停止条件
+def train(x, k, iters):
+    """
+    循环迭代
+    input:
+        x:样本集,m*d,其中m为样本数,d为样本维度数
+        k:聚类个数
+        iters:迭代次数
+    return:
+        ClusterLabel:最终的聚类结果
+    """
+    #初始化参数
+    AlphaArr0, MiuArr0, SigmaArr0 = initParas(x, k)
+    LLvalue0 = 0                #初始似然函数值
+    LLvalueList = []            #最大似然值列表
+    for i in range(iters):
+        #计算高斯分布模型的后验概率，也就是已知观测下来自于第k个高斯分布函数的概率
+        GamaProbArr = Gama_Prob(x, AlphaArr0, MiuArr0, SigmaArr0)
+        #计算聚类结果
+        ClusterLabel = np.argmax(GamaProbArr, axis=1)
+        #画分布图
+        drawPics(x, MiuArr0, SigmaArr0, ClusterLabel)
+        #计算似然函数，并判断是否继续更新
+        
+        #继续迭代，更新函数参数
+        MiuArr1, SigmaArr1, AlphaArr1 = updateParas(x, GamaProbArr)
+        MiuArr0 = np.copy(MiuArr1)
+        SigmaArr0 = np.copy(SigmaArr1) 
+        AlphaArr0 = np.copy(AlphaArr1)
 
 
 
-#外循环：判断是否符合停止条件
 
 
 
-#内循环：计算和更新各种参数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
